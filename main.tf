f26c08b1031fde053bff574e02ab0ca49ee18db7
Karan Bisht <82514836+Karan554@users.noreply.github.com>
2022-07-22 00:09:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,12 +7,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = var.aws_region
-  access_key = "AKIA4OYLAMLVES527IUD"
-  secret_key = "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"
-}
-
 resource "aws_security_group" "jenkins_sg" {
   name        = "jenkins_sg"
   description = "Allow Jenkins Traffic"
@@ -33,53 +27,6 @@ resource "aws_security_group" "jenkins_sg" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-}
-
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-2.0*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  owners = ["amazon"] # Canonical
-}
-
-
-resource "aws_instance" "web" {
-  ami             = data.aws_ami.amazon_linux.id
-  instance_type   = "t2.micro"
-  key_name        = var.key_name
-  security_groups = [aws_security_group.jenkins_sg.name]
-  tags = {
-    Name = "Test"
-  }
-
-  # Copies all files and folders in apps/app1 to D:/IIS/webapp1
-  provisioner "file" {
-    source      = "files/"
-    destination = "/home/ec2-user"
-  }
-
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    private_key = file("./vir.pem")
-    host     = "${aws_instance.web.public_ip}"
-  }
-
-
 }
 
 
