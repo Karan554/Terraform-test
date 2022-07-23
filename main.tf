60ca407d76291030a1e30fa619fe73d8dd1c7e17
Karan Bisht <82514836+Karan554@users.noreply.github.com>
2022-07-24 00:23:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,6 @@ terraform {
 
 provider "aws" {
    region = "us-east-1"
-   access_key = ${env.AWS_ACCESS_KEY_ID}
-   secret_key = ${env.AWS_SECRET_ACCESS_KEY}
 }
 
 resource "aws_security_group" "jenkins_sg" {
